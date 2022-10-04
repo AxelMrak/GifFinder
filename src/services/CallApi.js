0b28920075ee9889397447c7c85d32d60d305659
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2022-10-04 17:48:17 -0300

    Update CallApi.js

--- src/services/CallApi.js
+++ src/services/CallApi.js
@@ -4,24 +4,24 @@
  * @param { Receive the keyword, this keyword will passed to URL } param0 
  * @returns The fetch and the response of API
  */
-export async function CallApi( { keyword = 'cats' } ) {
+export async function CallApi({ keyword = 'cats' }) {
 
     // URL provide by Giphy
     const url = `https://api.giphy.com/v1/gifs/search?api_key=otRpzRdhk2LbJmF3ERUezAHFt9ElNef8&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
 
     // Call to URL and receive a response and this is transformed to JSON, after is transforme to array.  
-    return fetch(url)
-        .then( response => response.json() )
-        .then( response => {
+    return await fetch(url)
+        .then(response => response.json())
+        .then(response => {
             const { data = [] } = response;
             // We will access to data provide by API.
-            const gifs = data.map(gif => gif)
-            return gifs
-         })
-         // Takes or catch the errors
-         .catch( e => 
+            const gifs = data.map(gif => gif);
+            return gifs;
+        })
+        // Takes or catch the errors
+        .catch(e =>
             console.error('Error:' + e)
-         )
+        )
 
 }
 
